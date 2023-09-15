34ef7203ca09e3e1d6ca9a07b3638ad138cd98d0
rake005 <rakeshchandra005@gmail.com>
2023-09-15 11:08:15 +0530

    Typist

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import '../src'
+
+function App() {
+  return (
+    <div className="test">
+      <h1 style={{display:"flex"}}>Test Page</h1>
+    </div>
+  );
+}
+
+export default App;
